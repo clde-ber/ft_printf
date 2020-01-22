4404e8ecd0537134500e053fe65b146eff3301fa
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-22 16:26:12 +0100

    Update ft_printf_utils.c

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,19 +1,238 @@
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <stddef.h>
-# include <stdarg.h>
+int		check_format(const char *format)
+{
+	const char *c;
+	size_t i;
 
-int	ft_printf(const char *format, ...);
-int		check_format(const char *format);
-int	ft_isdigit(int c);
-int	ft_strlen(const char *s);
-const char	*ft_strjoin(const char *s1, const char *s2);
-const char	*find_param(const char *format, const char *params);
-const void	*ft_memmove(const void *dst, const void *src, size_t len);
-const char		*ft_itoa(int n);
-int	ft_atoi(const char *str);
+	i = 0;
+	c = format;
+	i = (c[i] == '%') ? ++i : 0;
+	if (i == 0)
+		return (0);
+	while (c[i] == 'd' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'x' || c[i] == 'X'
+		|| c[i] == 'c' || c[i] == 's' || c[i] == '*' || c[i] == '#' || c[i] == '0' || c[i] == '-'
+		|| ft_isdigit(c[i]) || c[i] == '$')
+	{
+		i = (c[i] == '*' || c[i] == '#' || c[i] == '0' || c[i] == '-' || ft_isdigit(c[i])) ? ++i : i;
+		i = (c[i] == 'd' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'x'
+		|| c[i] == 'X' || c[i] == 'c' || c[i] == 's' || ft_isdigit(c[i])) ? ++i : i;
+		i = (c[i] == '*') ? ++i : i;
+		i = (ft_isdigit(c[i])) ? ++i : i;
+		i = (c[i] == '$') ? ++i : i;
+		i = (c[i] == 'd' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'x' || c[i] == 'X'
+		|| c[i] == 'c' || c[i] == 's') ? ++i : i;
+		i = (c[i] == '\n') ? ++i : i;
+	}
+	if (i == ft_strlen(c))
+		return (i);
+	return (0);
+}
 
-#endif
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+const char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = -1;
+	j = -1;
+	if (!s1 || !s2)
+		return (0);
+	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (0);
+	while (s1[++i])
+		ptr[i] = s1[i];
+	while (s2[++j])
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+//	free (s2);
+//	free(s1);
+	return ((const char *)ptr);
+}
+
+const char	*find_param(const char *format, const char *params)
+{
+	size_t find_arg;
+	int i;
+
+	find_arg = 1;
+	i = -1;
+	while (format[++i])
+		if (ft_isdigit(format[i]))
+		{
+			find_arg = format[i] - 48;
+			break ;
+		}
+	i = -1;
+		while (params[++i + 1] && find_arg > 1)
+			find_arg = (params[i] == ',' && params[i + 1] == ' ') ? --find_arg : find_arg;
+		params = (const char *)ft_memmove(params, &params[i + 1], i);
+		i = 0;
+		while(params[i++])
+			if (params[i] == '\0' || params[i] == ',')
+			((char *)params)[i] = '\0';
+		return (params[0] == '\0') ? 0 : params;
+}
+
+const void	*ft_memmove(const void *dst, const void *src, size_t len)
+{
+	unsigned char	*tmp_src;
+	unsigned char	*tmp_dst;
+	size_t			i;
+
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	if (src <= dst)
+	{
+		tmp_dst = (unsigned char *)dst;
+		tmp_src = (unsigned char *)src;
+		while (len--)
+			tmp_dst[len] = tmp_src[len];
+	}
+	else
+	{
+		while (i < len)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	return ((const char *)dst);
+}
+
+static int	len_int(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == -2147483648)
+		return (11);
+	else if (n <= 2147483647 && n > -2147483648)
+	{
+		if (n == 0)
+			return (1);
+		if (n < 0)
+		{
+			n = -n;
+			i += 1;
+		}
+		while (n > 0)
+		{
+			n = n / 10;
+			i++;
+		}
+	}
+	return (i);
+}
+
+static int	ft_pow(int nb, int pow)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 1;
+	while (i < pow)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
+}
+
+const char		*ft_itoa(int n)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	if (n == -2147483648)
+		return ((const char *)ft_strjoin("-2147483648", ""));
+	i = -1;
+	k = n;
+	j = len_int(k);
+	if (!(str = malloc(sizeof(char) * (len_int(n) + 1))))
+		return (0);
+	n = (n < 0) ? -n : n;
+	while (j-- > 0)
+		if (i++ <= len_int(k))
+			str[j] = ((n / ft_pow(10, i)) % 10) + '0';
+	str[len_int(k)] = '\0';
+	str[0] = (k < 0) ? '-' : str[0];
+	return ((const char *)str);
+}
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int j;
+	int res;
+
+	i = 0;
+	j = 0;
+	res = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			j = (str[i] == '-') ? -1 : 1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+		res = res * 10 + (str[i++] - 48);
+	if (res == -2147483648)
+		return (res);
+	if (j < 0)
+		res = res * j;
+	return (res);
+}
+
+size_t	ft_nb_params(const char *format)
+{
+	size_t i;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' || format[i] == '*')
+			nb_params++;
+		i++;
+	}
+	return (nb_params);
+}
+
+char	ft_find_type(char *str)
+{
+	size_t i;
+	int boolean;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' || str[i] == '*' && ft_isdigit(str[i + 1]) == 0)
+			if (str[i + 1];
+		i++;
+	}
