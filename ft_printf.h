3131297d36f6f590c0fe72fbd8674fe50fb1ec67
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-22 08:50:32 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -3,14 +3,16 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-#include <stddef.h>
+# include <stddef.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *format, const char *params);
+int	ft_printf(const char *format, ...);
 int		check_format(const char *format);
 int	ft_isdigit(int c);
-size_t	ft_strlen(const char *s);
+int	ft_strlen(const char *s);
 const char	*ft_strjoin(const char *s1, const char *s2);
 const char	*find_param(const char *format, const char *params);
 const void	*ft_memmove(const void *dst, const void *src, size_t len);
+va_list		ft_convert(va_list args);
 
 #endif
