e6c7af6253bdde9ef769e06bc0005ba8b5d388fe
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-23 11:37:47 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -8,7 +8,7 @@
 # include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
-int		check_format(const char *format);
+int		ft_format(const char *format);
 int	ft_isdigit(int c);
 int	ft_strlen(const char *s);
 const char	*ft_strjoin(const char *s1, const char *s2);
@@ -16,5 +16,9 @@ const char	*find_param(const char *format, const char *params);
 const void	*ft_memmove(const void *dst, const void *src, size_t len);
 const char		*ft_itoa(int n);
 int	ft_atoi(const char *str);
+size_t	ft_nb_params(const char *format);
+char	ft_find_type(const char *str);
+size_t	ft_index(const char *str, const char *format);
+char	*char_to_s(int x);
 
 #endif
