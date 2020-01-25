c1ec3c5a0d8d6dc309d0694169bcd0f11947b804
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-25 09:15:20 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -3,20 +3,32 @@
 int	ft_printf(const char *format, ...)
 {
 	const char *str;
+	char **test;
+	char *n_format;
+	const char **tab;
 	size_t i;
+	size_t j;
 	va_list	args;
 
+	test = ft_rewrite_format(format);
+	printf("%s\n", test[0]);
+	printf("%s\n", test[1]);
+	printf("%s\n", test[2]);
+	n_format = ft_replace_format(ft_rewrite_format(format), format, n_format);
 	i = 0;
+	j = 0;
 	va_start(args, format);
 	str = ft_strjoin("", "");
 	while (i < ft_strlen(format) && ft_format(format))
 	{
 		str = extract_arg(i, str, args, format);
 		i += ft_index(&format[i], format);
-		str = (i != ft_strlen(format)) ? ft_strjoin(str, ", ") : str;
+		str = (i != ft_strlen(format)) ? ft_strjoin(str, ",") : str;
+		j++;
 		printf("%zu\n", i);
 		printf("str %s\n", str);
 	}
+	tab = ft_order_params(j, str, format);
 	va_end(args);
 	return (0);
 }
