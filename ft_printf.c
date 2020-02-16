e4d270a73792b06331b9eeb035e7bdb530ff3e72
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-02-16 19:15:20 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,6 +6,7 @@ int	ft_printf(const char *format, ...)
 	int j;
 	int x;
 	int ret;
+	int value;
 	int nb_args;
 	va_list args;
 	char **tab;
@@ -15,6 +16,7 @@ int	ft_printf(const char *format, ...)
 	x = 0;
 	nb_args = 0;
 	ret = 0;
+	value = 0;
 	while (format[j])
 	{
 		if (format[j] == '%')
@@ -31,11 +33,12 @@ int	ft_printf(const char *format, ...)
 		j++;
 		tab[i] = (char *)extract_arg(j, args, format);
 	//	printf("1 - tab[i]%s\n", tab[i]);
+	//	printf("i = %d\n", i);
 		if (format[j] != '%' || (format[j] == '%' && format[j + 1] == '%'))
 			i++;
-		if (format[j] == '%' && (format[j + 1] == '*' || format[j + 1] == '.') && (format[j + 2] == 'c' || format[j + 2] == 's' ||
+		if ((format[j] == '%' && format[j + 1] == '0') || (format[j] == '%' && (format[j + 1] == '*' || format[j + 1] == '.') && (format[j + 2] == 'c' || format[j + 2] == 's' ||
 			format[i + 2] == 'd' || format[i + 2] == 'i' || format[i + 2] == 'u' || format[i + 2] == 'p'
-			|| format[i + 2] == 'x' || format[i + 2] == 'X'))
+			|| format[i + 2] == 'x' || format[i + 2] == 'X')))
 		{
 			i++;
 			j++;
@@ -50,9 +53,20 @@ int	ft_printf(const char *format, ...)
 	//	printf("i%d\n", i);
 		if (tab[i][0] == 'f' && (tab[i][1] == '0' || tab[i][1] == '.' || tab[i][1] == '*' || tab[i][1] == '-'))
 		{
-			j += find_arg(j, tab);
-			tab[j] = ft_modify(j, tab, tab[i]);
+		//	printf("tab i %s\n", tab[i]);
+			j = find_arg(j, tab);
+			if (tab[i][2] == '0' && ft_strlen(tab[i]) > 3)
+			{
+				value = ft_atoi(&tab[i][3]);
+			//	printf("&tab[i][3] %s\n", &tab[i][3]);
+			//	printf("value = %d\n", value);
+				tab[j] = replace_spaces(value, ft_spaces(value - ft_strlen(tab[j]), tab[j]));
+			}
+			else
+				tab[j] = ft_modify(j, tab, tab[i]);
 		}
+		else
+			j++;
 		while (tab[i][x])
 		{
 			if (tab[i][x] != '@' && tab[i][0] != 'f')
