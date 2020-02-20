0cdc958f9d97e6e553608bde2de5c29dd9b0514e
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-02-20 11:30:27 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -4,32 +4,30 @@ int	ft_printf(const char *format, ...)
 {
 	int i;
 	int ret;
-	int boolean;
 	va_list args;
 	t_flag help;
 
 	i = 0;
 	ret = 0;
-	boolean = 0;
 	init_struct(&help);
 	va_start(args, format);
 	while (i < ft_strlen(format))
 	{
 		if (i == ft_strlen(format) - 1)
 			break ;
-		while (format[i] && !(find_percent(&format[i])))
+//		i += find_percent(&format[i]);
+	//	printf("findpercent %d\n", find_percent(&format[i]));
+		while (format[i] && (find_percent(&format[i]) > 1 || !find_percent(&format[i])))
 		{
 			ft_putchar(format[i]);
 			help.ret++;
-			i++;
+			i = (find_percent(&format[i])) ? i + find_percent(&format[i]) : ++i;
 		}
-		if (find_percent(&format[i]) && boolean == 0)
+		if (format[i] && find_percent(&format[i]) == 1)
 		{
 			fill_struct(&help, i, &format[i], args);
 			i = find_index(++i, format);
-			boolean = 1 ;
 		}
-		boolean = 0;
 	}
 	va_end(args);
 	return (help.ret);
