3054e861197e5fe5c7c7affe59f6244ffb1b8acc
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-02-21 18:04:13 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -11,15 +11,16 @@ typedef struct	s_flag
 {
 		int		width;
 		int		precision;
+		int		set_width;
+		int		set_prec;
 		int		rev;
 		int		zero;
 		int 	ret;
 }				t_flag;
 
 int			ft_printf(const char *format, ...);
-int 		ft_putstr_len(const char *str, t_flag *help);
-int ft_putstr(const char *str);
-void ft_putchar(char c);
+void 		ft_putstr_len(const char *str, t_flag *help);
+void ft_putchar(char c, t_flag *help);
 char	*to_hex(char c, unsigned int n, char *base);
 int	ft_atoi(const char *str);
 const char		*ft_itoa_u(unsigned int n);
@@ -30,13 +31,15 @@ const char	*ft_strjoin(const char *s1, const char *s2);
 int	ft_strlen(const char *s);
 int	ft_isdigit(int c);
 void fill_struct_conv(t_flag *help, char c, va_list args);
-void fill_struct(t_flag *help, int i, const char *format, va_list args);
+void fill_struct(t_flag *help, const char *format, va_list args);
 int find_index(int i, const char *str);
 int is_conv(const char c);
 int is_flag(const char c);
 int		find_percent(const char *str);
 void		init_struct(t_flag *help);
+void		init_all_except_ret(t_flag *help);
 char *str_toupper(char *str);
 char	*revstr(char *str);
+char *char_to_s(char c);
 
 #endif
