c88c1cb55b55f27c366b88cb1fa95f61f4dc75bf
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-02-22 17:14:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,64 @@
 #include "ft_printf.h"
 int main()
 {
-	ft_printf("%d%X%x\n", 25631654, 1000000, 2358);
-	printf("%d%X%x\n", 25631654, 1000000, 2358);
-	printf("%d\n", ft_printf("ultimate %*.*d     *.*s\n", 1, 50, 5000, 1, 0, "hey"));
-	printf("%d\n", printf("ultimate %*.*d     *.*s\n", 1, 50, 5000, 1, 0, "hey"));
-	printf("ret = %d\n", printf("%%p::[%1.50d]\n", 8473));
-	printf("ret = %d\n", ft_printf("%%p::[%1.50d]\n", 8473));
+	char c;
+
+	c = 'a';
+//	ft_printf("%d%X%x\n", 25631654, 1000000, 2358);
+//	printf("%d%X%x\n", 25631654, 1000000, 2358);
+//	printf("%d\n", ft_printf("ultimate %*.*d     *.*s\n", 1, 50, 5000, 1, 0, "hey"));
+//	printf("%d\n", printf("ultimate %*.*d     *.*s\n", 1, 50, 5000, 1, 0, "hey"));
+//	printf("ret = %d\n", printf("%%p::[%1.50d]\n", 8473));
+//	printf("ret = %d\n", ft_printf("%%p::[%1.50d]\n", 8473));
+//	printf("%d\n\n", printf("6pointeur 1 %012p pointeur 2 %012p\n\n", &c, &c));
+//	ft_printf("%d\n\n", printf("6pointeur 1 %012p pointeur 2 %012p\n\n", &c, &c));
+//	printf("st121 %*s\n", 100, "hello");
+//	ft_printf("st121 %*s\n", 100, "hello");
+//	printf("p31 %.4u\n", 100);
+//	ft_printf("p31 %.4u\n", 100);
+//	printf("p36 %.0u\n", 0);
+//	ft_printf("p36 %.0u\n", 0);
+//	printf("%d\n", printf("6hexa-maj 1 %012X hexa-maj 2 %012X\n", 42, -42));
+//	ft_printf("%d\n", printf("6hexa-maj 1 %012X hexa-maj 2 %012X\n", 42, -42));
+//	printf("neg11 %*.*d\n", -100, 15, 0);
+//	ft_printf("neg11 %*.*d\n", -100, 15, 0);
+	printf("%d\n", ft_printf("%c\n", '\0'));
+	printf("%d\n", printf("%c\n", '\0'));
+//	printf("%d\n", ft_printf("%5c\n", '\0'));
+//	printf("%d\n", printf("%5c\n", '\0'));
+//	printf("%d\n", ft_printf("%-5c\n", '\0'));
+//	printf("%d\n", printf("%-5c\n", '\0'));
+//	printf("%%\n");
+//	ft_printf("%%\n");
+//	printf("Hello 42 school! %s\n", NULL);
+//	ft_printf("Hello 42 school! %s\n", NULL);
+//	printf("%010%\n");
+//	ft_printf("%010%\n");
+//	printf("%d\n", 2147483647);
+//	ft_printf("%d\n", 2147483647);
+//	printf("%x\n", 2147483647);
+//	ft_printf("%x\n", 2147483647);
+//	printf("%X\n", 2147483647);
+//	ft_printf("%X\n", 2147483647);
+//	printf("%x\n", 0);
+//	ft_printf("%x\n", 0);
+//	printf("%010x\n", 0);
+//	ft_printf("%010x\n", 0);
+//	printf("%010x\n", 20);
+//	ft_printf("%010x\n", 20);
+//	printf("%010x\n", -20);
+//	ft_printf("%010x\n", -20);
+
+
 //	printf("%d\n\n", printf("15unsigned 1 %.u unsigned 2 %.u\n\n", 42, -42));
 //	printf("%d\n\n", ft_printf("15unsigned 1 %.u unsigned 2 %.u\n\n", 42, -42));
-/*	i = 400000;
-	printf("%d", i);
-	printf("sizeof int %d\n", sizeof(int));
-	printf("sizeof char %d\n", sizeof(char));
-	printf("sizeof char * %d\n", sizeof(char *));
-	printf("sizeof uint %d\n", sizeof(unsigned int));
-	printf("sizeof ulong %d\n", sizeof(unsigned long));
-	printf("sizeof unsigned char %d\n", sizeof(unsigned char));
-*/
+//	i = 400000;
+//	printf("%d", i);
+//	printf("sizeof int %d\n", sizeof(int));
+//	printf("sizeof char %d\n", sizeof(char));
+//	printf("sizeof char * %d\n", sizeof(char *));
+//	printf("sizeof uint %d\n", sizeof(unsigned int));
+//	printf("sizeof ulong %d\n", sizeof(unsigned long));
+//	printf("sizeof unsigned char %d\n", sizeof(unsigned char));
 	return (0);
-}/*
-int		main(int ac, char **av)
-{
-	(void)ac;
-	size_t i;
-	const char *format = "%s\n";
-	const char *params;
-	i = 1;
-	while (av[i + 1])
-	{
-		params = (i == 1) ? ft_strjoin(av[i], av[i + 1]) : ft_strjoin(params, av[i + 1]);
-//		printf(format, params);
-		i++;
-	}
-	ft_printf(format, "hello", "toi", "ca", "va");
-//	printf("params %s\n", params);
-	return (0);
-}*/
+}
