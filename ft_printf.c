66a1e9e8d202047cf12de389dd6353c44094e5b0
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-02-23 10:02:31 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -3,38 +3,30 @@
 int	ft_printf(const char *format, ...)
 {
 	int i;
-	int boolean;
 	va_list args;
 	t_flag help;
 
 	i = 0;
-	boolean = 0;
 	init_struct(&help);
 	va_start(args, format);
 	while (find_index(i, format) != -1)
 	{
 	//	printf("i = %d\n", i);
-		if (format[i] == '%' && format[i + 1] != '%' && boolean == 0)
+		if (format[i] == '%' && format[i + 1] != '%')
 		{
 			fill_struct(&help, &format[i], args);
 			i = find_index(++i, format);
 			init_all_except_ret(&help);
-			boolean = 1;
 		}
 		else
 		{
 			if (format[i] == '%' && format[i + 1] == '%')
 			{
 				i++;
-				write(1, &format[i], sizeof(char));
-				help.ret++;
+				help.ret += ft_putchar(format[i], &help);
 			}
 			if (format[i] != '%')
-			{
-				write(1, &format[i], sizeof(char));
-				help.ret++;
-			}
-			boolean = 0;
+				help.ret += ft_putchar(format[i], &help);
 			i++;
 		}
 	}
