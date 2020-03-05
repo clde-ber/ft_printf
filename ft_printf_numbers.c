36b7846973189a1a191ae19d1317e0a66eda9635
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-03-05 13:41:42 +0100

    Create ft_printf_numbers.c

---
+++ ft_printf_numbers.c
@@ -0,0 +1,88 @@
+int	ft_atoi(const char *str)
+{
+	int i;
+	int j;
+	int res;
+
+	i = 0;
+	j = 0;
+	res = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			j = (str[i] == '-') ? -1 : 1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+		res = res * 10 + (str[i++] - 48);
+	if (res == -2147483648)
+		return (res);
+	if (j < 0)
+		res = res * j;
+	return (res);
+}
+
+static int	len_int(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == -2147483648)
+		return (11);
+	else if (n <= 2147483647 && n > -2147483648)
+	{
+		if (n == 0)
+			return (1);
+		if (n < 0)
+		{
+			n = -n;
+			i += 1;
+		}
+		while (n > 0)
+		{
+			n = n / 10;
+			i++;
+		}
+	}
+	return (i);
+}
+
+static int	ft_pow(int nb, int pow)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 1;
+	while (i < pow)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
+}
+
+const char		*ft_itoa(int n)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	if (n == -2147483648)
+		return ((const char *)ft_strjoin("-2147483648", ""));
+	i = -1;
+	k = n;
+	j = len_int(k);
+	if (!(str = malloc(sizeof(char) * (len_int(n) + 1))))
+		return (0);
+	n = (n < 0) ? -n : n;
+	while (j-- > 0)
+		if (i++ <= len_int(k))
+			str[j] = ((n / ft_pow(10, i)) % 10) + '0';
+	str[len_int(k)] = '\0';
+	str[0] = (k < 0) ? '-' : str[0];
+	return ((const char *)str);
+}
