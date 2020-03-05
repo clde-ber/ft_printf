33dc4fe0db360fa0ca396cc2440767810c40b5e2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-03-05 13:39:39 +0100

    Create ft_printf_basics.c

---
+++ ft_printf_basics.c
@@ -0,0 +1,75 @@
+#include "ft_printf.h"
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return (6);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char *str_toupper(char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+const char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = -1;
+	j = -1;
+	if (!s1 || !s2)
+		return (0);
+	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (0);
+	while (s1[++i])
+		ptr[i] = s1[i];
+	while (s2[++j])
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return ((const char *)ptr);
+}
+
+const char	*join_a_free(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = -1;
+	j = -1;
+	if (!s1 || !s2)
+		return (0);
+	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (0);
+	while (s1[++i])
+		ptr[i] = s1[i];
+	while (s2[++j])
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	free((char *)s1);
+	return ((const char *)ptr);
+}
