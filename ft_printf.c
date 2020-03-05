c1939b20fbd511e8a8dc277a4693667f876d45ee
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-03-05 13:39:12 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,58 @@
 #include "ft_printf.h"
 
+void		init_struct(t_flag *help)
+{
+	help->width = 0;
+	help->precision = 0;
+	help->set_prec = 0;
+	help->rev = 0;
+	help->zero = 0;
+	help->ret = 0;
+	help->bool_1 = 0;
+	help->bool_2 = 0;
+	help->bool_3 = 0;
+	help->waste = 0;
+}
+
+void		init_all_except_ret(t_flag *help)
+{
+	help->width = 0;
+	help->precision = 0;
+	help->set_prec = 0;
+	help->rev = 0;
+	help->zero = 0;
+	help->bool_1 = 0;
+	help->bool_2 = 0;
+	help->bool_3 = 0;
+	help->waste = 0;
+}
+
+int find_index(int i, const char *str)
+{
+	while (str[i])
+	{
+		//printf("I (in find index) = %d\n", i);
+		if (is_flag(str[i]) == 0 && is_conv(str[i]) == 0)
+			return (i);
+		i++;
+	}
+	if (str[i] == '\0')
+		return (-1);
+	return (0);
+}
+
+void fill_struct_conv(t_flag *help, char c, va_list args)
+{
+	if (c == 's' || c == 'c')
+		manage_characters(help, c, args);
+	else if (c == 'd' || c == 'i' || c == 'u')
+		manage_numbers(help, c, args);
+	else if (c == 'x' || c == 'X')
+		manage_hex(help, c, args);
+	else if (c == 'p')
+		manage_pointers(help, c, args);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	int i;
@@ -9,27 +62,20 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	init_struct(&help);
 	va_start(args, format);
-	while (find_index(i, format) != -1)
-	{
-	//	printf("i = %d\n", i);
+	while (format[i])
 		if (format[i] == '%' && format[i + 1] != '%')
 		{
 			fill_struct(&help, &format[i], args);
 			i = find_index(++i, format);
-			init_all_except_ret(&help);
 		}
 		else
 		{
 			if (format[i] == '%' && format[i + 1] == '%')
-			{
-				i++;
-				help.ret += ft_putchar(format[i], &help);
-			}
+			 	help.ret += write(1, &format[++i], sizeof(char));
 			if (format[i] != '%')
-				help.ret += ft_putchar(format[i], &help);
+				help.ret += write(1, &format[i], sizeof(char));
 			i++;
 		}
-	}
 	va_end(args);
 	return (help.ret);
 }
